6740528d92a0df4615e291f1e0cfb9e2dbcee1f7
FANTVIC <awolez77@gmail.com>
2026-07-05 14:07:37 +0100

    first upload

--- app/page.jsx
+++ app/page.jsx
@@ -11,19 +11,20 @@ export default function Home() {
         <div className={styles.Nabar}><Nabar /></div>
         <img className={styles.header_banner} src="/img/img2.webp" alt="Banners" />
         <div className={styles.bannerTxt}>
-          <h1> Welcome to Resurrection centre LP33- DE slink</h1>
+          <h1> Welcome to Resurrection centre LP33- DE sleek</h1>
           <div className="NavButton">More details.</div>
           </div>
           
-      </header>
-      <section className={styles.otherPrograms}>
-        <div className={styles.otherProgramsDIV}>
-          <h2>Cultivating Mental, Physical and Spiritual Growth</h2>
+      </header>     
+        <div className={styles.cultivation.mps}>
+        Cultivating Mental, Physical and Spiritual Growth         
           <div className="navbtn">
             Other programs
           </div>
         </div>
-      </section>
+        
+        
+      
       {/* flex box lesson on 21st june . */}
       <section className={styles.flexboxlesson}>
         <div className={styles.flexbox}>
