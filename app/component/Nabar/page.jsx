f67797e8744080ab88f8cdaaf3c5ca092d45fb69
FANTVIC <awolez77@gmail.com>
2026-09-07 11:09:54 +0100

    nabar positioning

--- app/component/Nabar/page.jsx
+++ app/component/Nabar/page.jsx
@@ -3,28 +3,26 @@ import styles from "./Nabar.module.css";
 const Nabar = () => {
     return ( 
         <div className={styles.navbackground}>
-            <h1>
+            
             <div className={styles.Nabar}>
-            <div className={styles.Maincontainer}>
-                <div className={styles.Navcontainer1}>
-                    <img className={styles.Navlogo} src="/img/logo1.png" alt="logo" />
-                    <div className={styles.Navlink}>
-                        <li>Home</li>
-                        <li>Departement</li>
-                        <li>Welcome center</li>
-                        <li>E-libary</li>
-                        <li>feedback</li>
-                        <li>Events</li>
+                <div className={styles.Maincontainer}>
+                    <div className={styles.Navcontainer1}>
+                        <img className={styles.Navlogo} src="/img/logo1.png" alt="logo" />
+                        <div className={styles.Navlink}>
+                            <li>Home</li>
+                            <li>Departement</li>
+                            <li>Welcome center</li>
+                            <li>E-libary</li>
+                            <li>feedback</li>
+                            
+                        </div>
                     </div>
-                </div>
-                <div className={styles.Navcontainer2}>
-                    <Search/>
-                    <div className="NavButton">Give Now</div>
-                </div>
-            </div>                   
-        </div>
-            </h1>
-
+                    <div className={styles.Navcontainer2}>
+                        <Search color="white"/>
+                        <div className="NavButton">Give Now</div>
+                    </div>
+                </div>                   
+            </div>
         </div>
         
         
